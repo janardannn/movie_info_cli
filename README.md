e45759f10d87d62efca1a783d71bc91f843472b1
jh070 <jonardonhazarika7@gmail.com>
2020-11-22 19:08:53 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+MOVIE INFO CLI
+
+Get movies info's such as IMDb, RottenTomatoes, Metacritic and Description directly from
+command line.
+
+HOW TO USE:
+type in command line:
+python main.py -m "Movie Name"
+
+*currently only one argument is available.
+*It uses requests module to do a google search so, small typos aint a big deal.
---
+++ main.py
@@ -0,0 +1,32 @@
+import requests as req
+import sys
+from bs4 import BeautifulSoup as bs
+
+def get_imdb(query):
+    page = req.get(f"https://google.com/search?q={query}")
+    if page.status_code == 200:
+        page = page.text
+    else:
+        return ('IMDb ratings not available!')
+    
+    s = bs(page,'html5lib')
+    try:
+        IMDb = s.find('div',class_="BNeawe s3v9rd AP7Wnd").get_text()
+    except AttributeError:
+        return None
+    IMDb = IMDb.split('\n')
+    IMDb[-1] = "Description: " + IMDb[-1]
+    return "\n".join(IMDb)
+            
+def main():
+
+    if len(sys.argv)>=3 and sys.argv[1]=="-m":
+        query = "+".join(sys.argv[2].split(" "))
+       # print(query)
+        res = get_imdb(query)
+        if res != None:
+            print(res)
+        else:
+            print('No valid search results!')
+
+main()
